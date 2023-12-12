37b157aca2463564e6178211baec7f266bf8885c
fridaAfilia <frida.afilia29@gmail.com>
2023-12-12 17:25:45 +0700

    edit controller welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends CI_Controller
+{
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +21,14 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		redirect('auth');
+	}
+	public function not_found()
+	{
+		$this->load->view('404');
+	}
+	public function not_authorized()
+	{
+		$this->load->view('401');
 	}
 }
